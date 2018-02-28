60566cdd9ae7a58814a40f448fbdf9705b696dd2
Louisgrxxn <louisgreen21@icloud.com>
2018-02-28 23:59:12 +0000

    Appeals

---
+++ contactthanks.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Vortex | Appeals</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h3>Vortex Project - Appeals</h3>
+			 
+			
+		<br /><br />
+		
+			
+		<h1>Your appeal has been submitted and sent to our appeals team!</h1><br />
+		
+		<p><a href="index.html">Back</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
