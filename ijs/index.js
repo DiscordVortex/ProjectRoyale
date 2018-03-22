19f2530c30f21e88e19d7b7326e1bfc65e6c038a
Louisgrxxn <louisgreen21@icloud.com>
2018-03-22 17:42:19 +0000

    offline

--- ijs/index.js
+++
@@ -1,4 +0,0 @@
-//other pens
-// direct msg https://codepen.io/genarocolusso/pen/vKWVLy
-// pokemon Go card https://codepen.io/genarocolusso/pen/QEQGda
-// product card https://codepen.io/genarocolusso/pen/XKMbpE
